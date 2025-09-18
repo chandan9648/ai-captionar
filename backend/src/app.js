bb2ff79c64a1a4bb61387956a79184fdde8e495d
chandan9648 <chandankkumar156@gmail.com>
2025-09-19 00:38:24 +0530

    Refactor app.js to improve CORS configuration and dotenv initialization

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,16 +2,23 @@ const express = require('express');
 const authRoutes = require('./routes/auth.routes');
 const postRoutes = require("./routes/post.routes");
 const cookieParser = require('cookie-parser');
-
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
 
 const app = express()
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors({
+    origin: 'https://ai-captionar.onrender.com', // frontend URL
+    credentials: true, // allow cookies to be sent
+}));
 
 //AUTH AND POST ROUTES
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
+
 
 
 module.exports = app;
