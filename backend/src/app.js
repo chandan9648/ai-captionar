d69d1549a3544bd894e00848f18657c56a83906a
chandan9648 <chandankkumar156@gmail.com>
2025-09-19 14:35:47 +0530

    Enable CORS configuration in app.js for frontend access

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,16 +2,16 @@ const express = require('express');
 const authRoutes = require('./routes/auth.routes');
 const postRoutes = require("./routes/post.routes");
 const cookieParser = require('cookie-parser');
-// const cors = require('cors');
+const cors = require('cors');
 
 
 const app = express()
 app.use(cookieParser());
 app.use(express.json());
-// app.use(cors({
-//     origin: 'https://ai-captionar.vercel.app', // frontend URL
-//     credentials: true, // allow cookies to be sent
-// }));
+app.use(cors({
+    origin: 'https://ai-captionar.vercel.app', // frontend URL
+    credentials: true, // allow cookies to be sent
+}));
 
 //AUTH AND POST ROUTES
 app.use('/api/auth', authRoutes);
